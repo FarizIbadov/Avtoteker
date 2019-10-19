1bfb706e732609c081e6b023e9d636ca112e3d60
Fariz Ibadov <farizibadov@Farizs-MacBook-Pro.local>
2019-10-19 20:52:39 +0400

    Front

--- Avtoteker_FRONTEND/assets/javascript/script.js
+++ Avtoteker_FRONTEND/assets/javascript/script.js
@@ -1,3 +1,11 @@
+let LastData =  '#result'
+let same = 'container-fluid mt-2'
+let active = ' active'
+
+
 $('.Searchbtn').on('click',function(){
-    console.log($(this).data('value'))
+    $(LastData).attr('class',same);
+    LastData = $(this).data('value')
+    $(LastData).attr('class',same + ' active')
+    
 })
