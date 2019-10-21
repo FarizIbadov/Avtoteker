faa139d62a146b485b6c1bde2414d12dd108b2e4
Fariz Ibadov <farizibadov@Farizs-MacBook-Pro.local>
2019-10-21 22:03:05 +0400

    front

--- Avtoteker_FRONTEND/assets/javascript/script.js
+++ Avtoteker_FRONTEND/assets/javascript/script.js
@@ -6,6 +6,6 @@ let active = ' active'
 $('.Searchbtn').on('click',function(){
     $(LastData).attr('class',same);
     LastData = $(this).data('value')
-    $(LastData).attr('class',same + ' active')
+    $(LastData).attr('class',same + active)
     
 })
